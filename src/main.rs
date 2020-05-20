168e1e3ba0bd4d12c6243f8495c69bca96b65232
Matthias Fauconneau <matthias.fauconneau@gmail.com>
2020-05-20 15:15:48 +0200

    Deserialize goals (times)

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
-use anyhow::*;
+use {fehler::throws, anyhow::Error, chrono::NaiveTime, serde::{Deserialize,Deserializer}};
 
-#[derive(Debug,serde::Deserialize)] struct Room { rent: u32, address: String }
+#[derive(Debug)] struct Time(NaiveTime); //chrono::DateTime<Local>);
+impl<'de> Deserialize<'de> for Time {
+    #[throws(D::Error)]
+    fn deserialize<D:Deserializer<'de>>(deserializer: D) -> Self {
+        Time(NaiveTime::parse_from_str(&String::deserialize(deserializer)?, "%H:%M").map_err(serde::de::Error::custom)?)
+    }
+}
 
-#[fehler::throws]
+#[derive(Debug,Deserialize)] struct Room { address: String, rent: u32 }
+#[derive(Debug,Deserialize)] enum Trip { From(Time), To(Time) }
+#[derive(Debug,Deserialize)] struct Goal { address: String, trips: Vec<Trip> }
+
+#[throws]
 fn main() {
-    let rooms : Vec<Room> = ron::de::from_reader(std::fs::File::open("../rooms")?)?;
-    println!("{:?}", rooms);
+    let rooms : Vec<Room> = ron::de::from_reader(std::fs::File::open("../rooms.ron")?)?;
+    let goals: Vec<Goal> = ron::de::from_reader(std::fs::File::open("../goals.ron")?)?;
+    println!("{:?} {:?}", rooms, goals);
 }
